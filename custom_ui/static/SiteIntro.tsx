1520e38e8e670429261c6e9dac58450ffa8b4524
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2025-04-05 00:00:04 +0100

    comment some stuff out

--- custom_ui/static/SiteIntro.tsx
+++ custom_ui/static/SiteIntro.tsx
@@ -2,15 +2,15 @@
 
 export default function SiteIntro() {
     return (
-        <article className={"p-6 "}>
-            <p className={"text-3xl font-[900]"}>I build web solutions for people who want stuff done properly.</p>
-            <ul className={"list-disc text-lg"}>
-               <div className={"ml-6"}>
-                   <li>I primarily work with web technologies like TypeScript and PHP.</li>
-                   <li>I have commercial experience developing SaaS products for companies in the aviation industry worldwide.</li>
-                   <li>I currently work as a Software Engineer for Quantavia, building the next big thing for air operations.</li>
-               </div>
-            </ul>
+        <article className={"py-6"}>
+            <p className={"text-4xl font-[900]"}>You&apos;ve reached my site. Hello!</p>
+            {/*<ul className={"list-disc text-lg"}>*/}
+            {/*   <div className={"ml-6"}>*/}
+            {/*       <li>I primarily work with web technologies like TypeScript and PHP.</li>*/}
+            {/*       <li>I have commercial experience developing SaaS products for companies in the aviation industry worldwide.</li>*/}
+            {/*       <li>I currently work as a Software Engineer for Quantavia, building the next big thing for air operations.</li>*/}
+            {/*   </div>*/}
+            {/*</ul>*/}
         </article>
     )
 }
