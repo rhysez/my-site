874bf860f6b639ea86540615b007d15668e943f1
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2025-04-15 20:36:18 +0100

    populate contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,30 @@
+import SectionHeader from "@/custom_ui/static/SectionHeader";
+import SectionTextBody from "@/custom_ui/static/SectionTextBody";
 
 export default function Page() {
     return (
-        <main>
-            <p className={"lora text-xl font-[300]"}>
-                Hmmm... nothing here yet.
-            </p>
-        </main>
+        <article className={"py-6 space-y-4"}>
+            <section>
+                <p className={"lora text-xl font-[300]"}>
+
+                </p>
+            </section>
+
+            <hgroup>
+                <SectionHeader text={"contact me"} />
+                <SectionTextBody>
+                    <p className={"lora text-base font-[300]"}>
+                       If you would like to contact me for freelance enquiries, please consider
+                        {" "}<a className={"underline hover:text-jade transition-all"} href={"mailto:rhyshodgsondev@gmail.com"}>sending me an email.</a>
+                    </p>
+
+                    <p className={"lora text-base font-[300]"}>
+                        If you would like to contact me for other professional opportunities, please consider
+                        {" "}<a className={"underline hover:text-jade transition-all"} href={"https://www.linkedin.com/in/rhys-hodgson-8262b3218/"}>connecting with me on LinkedIn.</a>
+                    </p>
+                </SectionTextBody>
+            </hgroup>
+
+        </article>
     )
 }
