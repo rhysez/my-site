f547afb2a3101b6b42b93bae18346036dd0c1e2d
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2025-04-30 22:40:39 +0100

    add /work page content

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
     return (
         <main>
             <p className={"lora text-xl font-[300]"}>
-               Hmmm... nothing here yet.
+               My blog isn&apos;t available yet, check back soon!
             </p>
         </main>
     )
